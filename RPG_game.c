dd7a74ee21518b1b2cb6c5d27072794febf6d118
Radoslaw <rylkoradoslaw230@gmail.com>
2021-10-05 01:14:33 +0200

    rewritten shit

--- RPG_game.c
+++ RPG_game.c
@@ -1,60 +1,68 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
+#include <curses.h>
+#include <stdlib.h>
 
-// int *
 
-void renderMap(char map_field[][7], int rows)
+
+
+struct player
 {
-    // printf("%d", map_field);
-    // char arr[3][7] = *map_field;
-    // char arr[3];
-    // memcpy(arr, *map_field, 7);
-    // char arr[3][7] = *map_field;
-    for (int k = 0; k < rows; k++)
-    {
-        printf("%s\n", map_field);
-    }
-}
+    int player_x;
+    int player_y;
+    char player;
+};
+
+
+
 
 int main()
 {
     bool run = true;
-    const int rows = 3;
-    const int columns = 7;
-    char player = '@';
-    int player_x = 0;
-    int player_y = 0;
-    // char empty_symbol[4] = "---";
-    char empty_line[] = "-";
-    char map_field[rows][columns];
-    // strcat(empty_line, empty_symbol);
-    // strcpy(map_field[0], "aa");
-    // printf("%s", map_field[0]);
-    for (int i = 0; i < rows; i++)
+    struct player player1 = {0, 0, '@'};
+    char direction;
+ 
+    char map[][10] = {
+        "---------", 
+        "---------",
+        "---------"
+    }; 
+    // adjusts width of map
+    while (run)
     {
-        strcpy(map_field[i], "-----");
-    }
-    printf("%s\n", map_field[0]);
-    printf("%s\n", map_field[1]);
-    printf("%s\n", map_field[2]);
-    // printf("%s\n", map_field[3]);
-    // printf("%s\n", map_field[1]);
-    // printf("%s\n", map_field[1]);
-    int cols = columns;
-    for (int f = 0; f < cols - 1; f++)
-    {
-        strcat(empty_line, "-");
-    }
-   
-    for (int j = 0; j < 2; j++)
-    {
-        printf("%s\n", map_field[j]);
-    }
-//   /* 
 
-    // loop which creates an array of chars to make map_field (without game character)
+        // printf("\e[1;1H\e[2J");
+        // direction = getch();
+        direction = getchar();
 
+
+        system("clear");
+       if (direction == 'w')
+       {
+           player1.player_y -= 1;
+       } else if (direction == 's')
+       {
+           player1.player_y += 1;
+       } else if (direction == 'd')
+       {
+           player1.player_x += 1;
+       } else if (direction == 'a')
+       {
+           player1.player_x -= 1;
+       }
+        for (int i = 0; i < 3; i++)
+        {
+            map[player1.player_y][player1.player_x] = player1.player;
+            printf("%s\n", map[i]);
+            map[player1.player_y][player1.player_x] = '-';
+        }      
+    }
+
+    // while (run)
+    // {
+    //     ;
+    // }
 
 
 
