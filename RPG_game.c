4aced54ed74cb35dc906d5878a74fb12e90dc80e
Radoslaw <rylkoradoslaw230@gmail.com>
2021-10-06 20:30:39 +0200

    checkPlayerBorders function

--- RPG_game.c
+++ RPG_game.c
@@ -3,15 +3,17 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-void refreshMap(int columns, int rows, char map[][columns])
+void refreshMap(int columns, int rows, char map[][columns], char empty_field[])
 {
+    // puts only empty spaces on maps
     for (int row = 0; row < rows; row++)
     {
-        strcpy(map[row], "------");
+        strcpy(map[row], empty_field);
     }
 }
 void renderMap(int columns, int rows, char map[][columns])
 {
+    // renders every row of a map array
     for (int row = 0; row < rows; row++)
     {
         printf("%s\n", map[row]);
@@ -25,11 +27,28 @@ void refreshScreen()
 
 void placePlayer(int columns, char map[][columns], int *player_x, int *player_y, char *player_icon)
 {
+    //places player on the map
     map[*player_y][*player_x] = *player_icon; 
 }
-
+void checkPlayerBorders(int *player_x, int *player_y, int map_rows, int map_columns)
+{
+    if (*player_x > map_columns - 2)
+    {
+        *player_x = map_columns - 2;
+    } else if (*player_x < 0)
+    {
+        *player_x = 0;
+    } else if (*player_y > map_rows - 1)
+    {
+        *player_y = map_rows - 1;
+    } else if (*player_y < 0)
+    {
+        *player_y = 0;
+    }
+}
 void movePlayer(int *player_x, int *player_y, char *direction)
 {
+    //changes the direction of player movement
     if (*direction == 'w')
     {
         *player_y -= 1;
@@ -45,8 +64,11 @@ void movePlayer(int *player_x, int *player_y, char *direction)
     }
 }
 
+
+
 char input_func(int OS_FLAG)
 {
+    // get input and returns it based on OS you're using Windows/Apple
     char input;
     if (OS_FLAG == 1)
     {
@@ -68,15 +90,24 @@ struct player
     char direction;
 };
 
+struct chest
+{
+    int chest_x;
+    int chest_y;
+    char chest;
+    int item_id;
+};
+
 int main()
 {
     bool run = true;
     struct player player1 = {0, 0, '@', 'n'};
     char direction;
-    int rows_int = 3;
+    int rows_int = 5;
     const int rows_const = rows_int;
-    const int columns_const = 20;
-    char empty_field[] = "-";
+    const int columns_const = 45;
+    char empty_field[columns_const];
+    strcpy(empty_field, "-");
     // 1 = WINDOWS, 0 = APPLE
     int OS_FLAG;
 
@@ -92,21 +123,26 @@ int main()
 
 
     char map[rows_const][columns_const];
-    for (int row = 0; row < rows_const; row++)
+    for (int column = 0; column < columns_const - 2; column++)
     {
-        strcpy(map[row], "------");
+        strcat(empty_field, "-");
     }
+    
+    printf("Empty field %s\n", empty_field);
+    // refreshMap(columns_const, rows_const, map, empty_field);
 
+    // mainloop
     while (run)
     {
+        
         placePlayer(columns_const, map, &player1.player_x, &player1.player_y, &player1.player);
         renderMap(columns_const, rows_const, map);
         player1.direction = input_func(OS_FLAG);
         movePlayer(&player1.player_x, &player1.player_y, &player1.direction);
+        checkPlayerBorders(&player1.player_x, &player1.player_y, rows_const, columns_const);
         refreshScreen();
-        refreshMap(columns_const, rows_const, map);
+        refreshMap(columns_const, rows_const, map, empty_field);
     }
-    getchar();
 
     return 0;
 }
